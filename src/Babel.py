c98cabbc1a144e3098b37974fbdf556e3d8e8636
Frosty-ID <frostysmile864@gmail.com>
2024-09-21 12:23:33 -0700

    Logging for Python, about to tests after some api calls

--- src/Babel.py
+++ src/Babel.py
@@ -1,14 +1,21 @@
+import logging
+
 class Babel:
     def __init__(self):
         self.languageServers = []
     
+    def serverPaid(self, langServer) -> bool:
+        return langServer.paid
+    
     def addLanguageServer(self, langServer):
-        if serverPaid(langServer):
+        if self.serverPaid(langServer):
             self.languageServers.append(langServer)
+            logging.info(f"Added language server: {langServer}")
         else:
-            print("")
+            logging.warning(f"Failed to add language server: {langServer} (not paid)")
     
     def server(self):
+        pass
         # some API call
     
     def provideServer(self):
--- src/Server.py
+++ src/Server.py
@@ -1,6 +1,14 @@
 class Server:
-    def __init__(self, name):
+    def __init__(self, name, paid=False):
         self.name = name
+        self.paid = False
     
-    def payMonthly(self):
+    def payMonthly(self) -> bool:
+        # api call with stripe perhaps
+        if pay $5 monthly:
+            self.paid = True
         
+        if self.paid:
+            return True
+        else:
+            return False

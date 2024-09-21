c85eb829a8d1ba75543eb8aadbd2ab032b6806d4
Frosty-ID <frostysmile864@gmail.com>
2024-09-21 15:03:52 -0700

    clean

--- api/googleApi.py
+++
@@ -1,9 +0,0 @@
-import os
-import google.generativeai as genai
-
-genai.configure(api_key=os.environ["API_KEY"])
-
-def call_google_ai(prompt):
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content("Write a story about a magic backpack.")
-    print(response.text)
